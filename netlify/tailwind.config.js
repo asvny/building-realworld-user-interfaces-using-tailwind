bb4a5ab1e6a8f0b606a24afa030f7c2dfd1b3f9b
asvny <annamalai.saro@gmail.com>
2019-11-22 08:45:54 +1100

    Add netlify files

---
+++ netlify/tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  theme: {
+    // fontFamily: {
+    //   sans: [
+    //     'Circular Std',
+    //     '-apple-system',
+    //     'BlinkMacSystemFont',
+    //     '"Segoe UI"',
+    //     'Roboto',
+    //     '"Helvetica Neue"',
+    //     'Arial',
+    //     '"Noto Sans"',
+    //     'sans-serif',
+    //     '"Apple Color Emoji"',
+    //     '"Segoe UI Emoji"',
+    //     '"Segoe UI Symbol"',
+    //     '"Noto Color Emoji"',
+    //   ]
+    // },
+    extend: {
+      colors: {
+        "dark-ocean": '#161f27'
+      },
+      width: {
+        680: '680px'
+      },
+      boxShadow: {
+        'highlight-menu': '3px 0 0 0 #0ba90b inset'
+      }
+    }
+  },
+  plugins: [
+    // function ({ addBase, _ }) {
+    //   addBase({
+    //     'input': { transition: 'border,background 0.2s ease-in' },
+    //   })
+    // },
+  ]
+}
